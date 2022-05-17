68c7140fe96714f012b48584318d3c882fc23288
DeluxeOwl <nospamplease0@protonmail.com>
2022-05-17 21:42:16 +0300

    feat: removed some nodes parsing

--- tree/tree.go
+++ tree/tree.go
@@ -31,36 +31,6 @@ func Walk(node ast.Node, f func(ast.Node)) {
 
 		if !shouldStop(current) {
 			switch n := current.(type) {
-			// Comments and fields
-			case *ast.Comment:
-				// nothing to do
-
-			case *ast.CommentGroup:
-				for _, c := range n.List {
-					queue = append(queue, c)
-				}
-
-			case *ast.Field:
-				if n.Doc != nil {
-					queue = append(queue, n.Doc)
-				}
-				for _, x := range n.Names {
-					queue = append(queue, x)
-				}
-				if n.Type != nil {
-					queue = append(queue, n.Type)
-				}
-				if n.Tag != nil {
-					queue = append(queue, n.Tag)
-				}
-				if n.Comment != nil {
-					queue = append(queue, n.Comment)
-				}
-
-			case *ast.FieldList:
-				for _, f := range n.List {
-					queue = append(queue, f)
-				}
 
 			// Expressions
 			case *ast.BadExpr, *ast.Ident, *ast.BasicLit:
@@ -75,14 +45,6 @@ func Walk(node ast.Node, f func(ast.Node)) {
 				queue = append(queue, n.Type)
 				queue = append(queue, n.Body)
 
-			case *ast.CompositeLit:
-				if n.Type != nil {
-					queue = append(queue, n.Type)
-				}
-				for _, x := range n.Elts {
-					queue = append(queue, x)
-				}
-
 			case *ast.ParenExpr:
 				queue = append(queue, n.X)
 
@@ -90,42 +52,11 @@ func Walk(node ast.Node, f func(ast.Node)) {
 				queue = append(queue, n.X)
 				queue = append(queue, n.Sel)
 
-			case *ast.IndexExpr:
-				queue = append(queue, n.X)
-				queue = append(queue, n.Index)
-
-			case *ast.IndexListExpr:
-				queue = append(queue, n.X)
-				for _, index := range n.Indices {
-					queue = append(queue, index)
-				}
-
-			case *ast.SliceExpr:
-				queue = append(queue, n.X)
-				if n.Low != nil {
-					queue = append(queue, n.Low)
-				}
-				if n.High != nil {
-					queue = append(queue, n.High)
-				}
-				if n.Max != nil {
-					queue = append(queue, n.Max)
-				}
-
-			case *ast.TypeAssertExpr:
-				queue = append(queue, n.X)
-				if n.Type != nil {
-					queue = append(queue, n.Type)
-				}
-
 			case *ast.CallExpr:
 				queue = append(queue, n.Fun)
 				for _, x := range n.Args {
 					queue = append(queue, x)
 				}
-
-			case *ast.StarExpr:
-				queue = append(queue, n.X)
 
 			case *ast.UnaryExpr:
 				queue = append(queue, n.X)
@@ -133,41 +64,6 @@ func Walk(node ast.Node, f func(ast.Node)) {
 			case *ast.BinaryExpr:
 				queue = append(queue, n.X)
 				queue = append(queue, n.Y)
-
-			case *ast.KeyValueExpr:
-				queue = append(queue, n.Key)
-				queue = append(queue, n.Value)
-
-			// Types
-			case *ast.ArrayType:
-				if n.Len != nil {
-					queue = append(queue, n.Len)
-				}
-				queue = append(queue, n.Elt)
-
-			case *ast.StructType:
-				queue = append(queue, n.Fields)
-
-			case *ast.FuncType:
-				if n.TypeParams != nil {
-					queue = append(queue, n.TypeParams)
-				}
-				if n.Params != nil {
-					queue = append(queue, n.Params)
-				}
-				if n.Results != nil {
-					queue = append(queue, n.Results)
-				}
-
-			case *ast.InterfaceType:
-				queue = append(queue, n.Methods)
-
-			case *ast.MapType:
-				queue = append(queue, n.Key)
-				queue = append(queue, n.Value)
-
-			case *ast.ChanType:
-				queue = append(queue, n.Value)
 
 			// Statements
 			case *ast.BadStmt:
@@ -179,200 +75,8 @@ func Walk(node ast.Node, f func(ast.Node)) {
 			case *ast.EmptyStmt:
 				// nothing to do
 
-			case *ast.LabeledStmt:
-				queue = append(queue, n.Label)
-				queue = append(queue, n.Stmt)
-
 			case *ast.ExprStmt:
 				queue = append(queue, n.X)
-
-			case *ast.SendStmt:
-				queue = append(queue, n.Chan)
-				queue = append(queue, n.Value)
-
-			case *ast.IncDecStmt:
-				queue = append(queue, n.X)
-
-			case *ast.AssignStmt:
-				for _, x := range n.Lhs {
-					queue = append(queue, x)
-				}
-				for _, x := range n.Rhs {
-					queue = append(queue, x)
-				}
-
-			case *ast.GoStmt:
-				queue = append(queue, n.Call)
-
-			case *ast.DeferStmt:
-				queue = append(queue, n.Call)
-
-			case *ast.ReturnStmt:
-				for _, x := range n.Results {
-					queue = append(queue, x)
-				}
-
-			case *ast.BranchStmt:
-				if n.Label != nil {
-					queue = append(queue, n.Label)
-				}
-
-			case *ast.BlockStmt:
-				for _, x := range n.List {
-					queue = append(queue, x)
-				}
-
-			case *ast.IfStmt:
-				if n.Init != nil {
-					queue = append(queue, n.Init)
-				}
-				queue = append(queue, n.Cond)
-				queue = append(queue, n.Body)
-				if n.Else != nil {
-					queue = append(queue, n.Else)
-				}
-
-			case *ast.CaseClause:
-				for _, x := range n.List {
-					queue = append(queue, x)
-				}
-				for _, x := range n.Body {
-					queue = append(queue, x)
-				}
-
-			case *ast.SwitchStmt:
-				if n.Init != nil {
-					queue = append(queue, n.Init)
-				}
-				if n.Tag != nil {
-					queue = append(queue, n.Tag)
-				}
-				queue = append(queue, n.Body)
-
-			case *ast.TypeSwitchStmt:
-				if n.Init != nil {
-					queue = append(queue, n.Init)
-				}
-				queue = append(queue, n.Assign)
-				queue = append(queue, n.Body)
-
-			case *ast.CommClause:
-				if n.Comm != nil {
-					queue = append(queue, n.Comm)
-				}
-				for _, x := range n.Body {
-					queue = append(queue, x)
-				}
-
-			case *ast.SelectStmt:
-				queue = append(queue, n.Body)
-
-			case *ast.ForStmt:
-				if n.Init != nil {
-					queue = append(queue, n.Init)
-				}
-				if n.Cond != nil {
-					queue = append(queue, n.Cond)
-				}
-				if n.Post != nil {
-					queue = append(queue, n.Post)
-				}
-				queue = append(queue, n.Body)
-
-			case *ast.RangeStmt:
-				if n.Key != nil {
-					queue = append(queue, n.Key)
-				}
-				if n.Value != nil {
-					queue = append(queue, n.Value)
-				}
-				queue = append(queue, n.X)
-				queue = append(queue, n.Body)
-
-			// Declarations
-			case *ast.ImportSpec:
-				if n.Doc != nil {
-					queue = append(queue, n.Doc)
-				}
-				if n.Name != nil {
-					queue = append(queue, n.Name)
-				}
-				queue = append(queue, n.Path)
-				if n.Comment != nil {
-					queue = append(queue, n.Comment)
-				}
-
-			case *ast.ValueSpec:
-				if n.Doc != nil {
-					queue = append(queue, n.Doc)
-				}
-				for _, x := range n.Names {
-					queue = append(queue, x)
-				}
-				if n.Type != nil {
-					queue = append(queue, n.Type)
-				}
-				for _, x := range n.Values {
-					queue = append(queue, x)
-				}
-				if n.Comment != nil {
-					queue = append(queue, n.Comment)
-				}
-
-			case *ast.TypeSpec:
-				if n.Doc != nil {
-					queue = append(queue, n.Doc)
-				}
-				queue = append(queue, n.Name)
-				if n.TypeParams != nil {
-					queue = append(queue, n.TypeParams)
-				}
-				queue = append(queue, n.Type)
-				if n.Comment != nil {
-					queue = append(queue, n.Comment)
-				}
-
-			case *ast.BadDecl:
-				// nothing to do
-
-			case *ast.GenDecl:
-				if n.Doc != nil {
-					queue = append(queue, n.Doc)
-				}
-				for _, s := range n.Specs {
-					queue = append(queue, s)
-				}
-
-			case *ast.FuncDecl:
-				if n.Doc != nil {
-					queue = append(queue, n.Doc)
-				}
-				if n.Recv != nil {
-					queue = append(queue, n.Recv)
-				}
-				queue = append(queue, n.Name)
-				queue = append(queue, n.Type)
-				if n.Body != nil {
-					queue = append(queue, n.Body)
-				}
-
-			// Files and packages
-			case *ast.File:
-				if n.Doc != nil {
-					queue = append(queue, n.Doc)
-				}
-				queue = append(queue, n.Name)
-				for _, x := range n.Decls {
-					queue = append(queue, x)
-				}
-				// don't walk n.Comments - they have been
-				// visited already through the individual
-				// nodes
-
-			case *ast.Package:
-				for _, f := range n.Files {
-					queue = append(queue, f)
-				}
 
 			default:
 				panic(fmt.Sprintf("ast.Walk: unexpected node type %T", n))
@@ -388,14 +92,14 @@ func Inspect(node ast.Node, f func(ast.Node)) {
 
 func main() {
 	fs := token.NewFileSet()
-	// tr, _ := parser.ParseExpr("reader & !writer")
-	// ast.Print(fs, tr)
+	tr, _ := parser.ParseExpr("reader & !writer")
+	ast.Print(fs, tr)
 
 	// tr, _ := parser.ParseExpr("reader | writer | !parent_folder.reader")
 	// ast.Print(fs, tr)
 
-	tr, _ := parser.ParseExpr("!parent_folder.reader | reader | writer")
-	ast.Print(fs, tr)
+	// tr, _ := parser.ParseExpr("!parent_folder.reader | reader | writer")
+	// ast.Print(fs, tr)
 
 	// tr, _ := parser.ParseExpr("(reader | writer) & !parent_folder.reader")
 	// ast.Print(fs, tr)
