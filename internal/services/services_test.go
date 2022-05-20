336885879bbaa9e4d53a645e55e9e1f5cf54941b
DeluxeOwl <nospamplease0@protonmail.com>
2022-05-20 19:52:33 +0300

    feat: added tests for permissions

--- internal/services/services_test.go
+++ internal/services/services_test.go
@@ -168,3 +168,252 @@ func TestSubjectCreation(t *testing.T) {
 	}
 
 }
+
+func TestPermissionCheck(t *testing.T) {
+	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	defer client.Close()
+
+	h := Handler{
+		Client: client,
+	}
+
+	ctx := context.Background()
+
+	h.CreateTypeConfig(ctx,
+		&models.TypeConfigReq{Name: "user"},
+	)
+
+	h.CreateTypeConfig(ctx,
+		&models.TypeConfigReq{
+			Name: "group",
+			Relations: map[string]string{
+				"member": "user",
+			},
+		})
+
+	h.CreateTypeConfig(ctx,
+		&models.TypeConfigReq{
+			Name: "folder",
+			Relations: map[string]string{
+				"reader": "user | group#member",
+			},
+		})
+
+	h.CreateTypeConfig(ctx,
+		&models.TypeConfigReq{
+			Name: "document",
+			Relations: map[string]string{
+				"parent_folder": "folder",
+				"writer":        "user",
+				"reader":        "user",
+			},
+			Permissions: map[string]string{
+				"read":           "reader | writer | parent_folder.reader",
+				"read_and_write": "reader & writer",
+				"read_only":      "reader & !writer",
+			},
+		})
+
+	subjects := []models.SubjectReq{
+		{
+			TypeConfigName: "document",
+			SubjectName:    "report.csv",
+		},
+		{
+			TypeConfigName: "user",
+			SubjectName:    "anna",
+		},
+		{
+			TypeConfigName: "user",
+			SubjectName:    "john",
+		},
+		{
+			TypeConfigName: "user",
+			SubjectName:    "steve",
+		},
+		{
+			TypeConfigName: "folder",
+			SubjectName:    "secret_folder",
+		},
+		{
+			TypeConfigName: "group",
+			SubjectName:    "dev",
+		},
+		{
+			TypeConfigName: "group",
+			SubjectName:    "test_group",
+		},
+	}
+
+	for _, v := range subjects {
+		h.CreateSubject(ctx, &v)
+	}
+
+	tuples := []models.TupleReqRelation{
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "anna",
+			},
+			Relation: "reader",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "document",
+				SubjectName:    "report.csv",
+			},
+		},
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "anna",
+			},
+			Relation: "writer",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "document",
+				SubjectName:    "report.csv",
+			},
+		},
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "folder",
+				SubjectName:    "secret_folder",
+			},
+			Relation: "parent_folder",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "document",
+				SubjectName:    "report.csv",
+			},
+		},
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "john",
+			},
+			Relation: "reader",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "folder",
+				SubjectName:    "secret_folder",
+			},
+		},
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "john",
+			},
+			Relation: "member",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "group",
+				SubjectName:    "dev",
+			},
+		},
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "group",
+				SubjectName:    "dev#member",
+			},
+			Relation: "reader",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "folder",
+				SubjectName:    "secret_folder",
+			},
+		},
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "group",
+				SubjectName:    "test_group#member",
+			},
+			Relation: "reader",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "folder",
+				SubjectName:    "secret_folder",
+			},
+		},
+		{
+			Subject: &models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "steve",
+			},
+			Relation: "member",
+			Resource: &models.SubjectReq{
+				TypeConfigName: "group",
+				SubjectName:    "dev",
+			},
+		},
+	}
+
+	for _, v := range tuples {
+		h.CreateTuple(ctx, &v)
+	}
+
+	resource := &models.SubjectReq{
+		TypeConfigName: "document",
+		SubjectName:    "report.csv",
+	}
+
+	tables := []struct {
+		subject    *models.SubjectReq
+		permission string
+		hasPerm    bool
+	}{
+		{
+			&models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "john",
+			}, "read", true,
+		},
+		{
+			&models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "anna",
+			}, "read", true,
+		},
+		{
+			&models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "steve",
+			}, "read", true,
+		},
+		{
+			&models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "anna",
+			}, "read_only", false,
+		},
+		{
+			&models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "anna",
+			}, "read_and_write", true,
+		},
+		{
+			&models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "steve",
+			}, "read_and_write", false,
+		},
+		{
+			&models.SubjectReq{
+				TypeConfigName: "user",
+				SubjectName:    "john",
+			}, "read_and_write", false,
+		},
+	}
+
+	for _, table := range tables {
+		hasPerm, err := h.CheckPermission(ctx, &models.TupleReqPermission{
+			Subject:    table.subject,
+			Permission: table.permission,
+			Resource:   resource,
+		})
+		if err != nil {
+			if err != nil {
+				t.Fatalf("at subject `%s` and permission `%s`, shouldn't get error %s",
+					table.subject.SubjectName, table.permission, err)
+			}
+			if hasPerm != table.hasPerm {
+				t.Errorf("at subject `%s` and permission %s, wanted `%t` got `%t`",
+					table.subject.SubjectName, table.permission, table.hasPerm, hasPerm)
+			}
+		}
+	}
+
+}
