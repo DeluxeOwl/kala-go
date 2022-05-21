32d41b2f8526621fd6c34c34cddee79754166be2
DeluxeOwl <nospamplease0@protonmail.com>
2022-05-21 16:33:45 +0300

    feat(gen): changed the way ent generates code

---
+++ ent/entc.go
@@ -0,0 +1,17 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+func main() {
+	if err := entc.Generate("./schema", &gen.Config{}); err != nil {
+		log.Fatal("running ent codegen:", err)
+	}
+}
