9f947066e572cf20ffdf2f6ce90f5b7704f037ce
DeluxeOwl <nospamplease0@protonmail.com>
2022-05-22 10:48:29 +0300

    feat: updated favicon

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App(props: AppProps) {
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          colorScheme: "light",
+          colorScheme: "dark",
         }}
       >
         <Component {...pageProps} />
